538a5b25aff2762549add49fe21e9dec51b644ba
Mohammed Amine El Jirari <med.amineeljirari@gmail.com>
2017-01-22 23:49:28 +0100

    Add POST request handling

--- app.js
+++ app.js
@@ -1,37 +1,66 @@
 var express = require('express'),
     app = express(),
     engines = require('consolidate'),
+    bodyParser = require('body-parser'),
     MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({extended: true}));
 
+// Handler for internal server errors
+function errorHandler(err, req, res, next) {
+    console.error(err.message);
+    console.error(err.stack);
+    res.status(500).render('error_template', {error: err});
+}
+
+// connect to db server
 MongoClient.connect('mongodb://localhost:27017/video', function (err, db) {
 
     assert.equal(null, err);
-    console.log("Successfully connected to MongoDB Server !");
+    console.log("Successfully connected to MongoDB.");
 
+    // Get request handler
     app.get('/', function (req, res) {
-
         db.collection('movies').find({}).toArray(function (err, docs) {
             res.render('movies', {'movies': docs});
         });
-
     });
 
-    app.use(function (req, res) {
-        res.sendStatus(404);
+    // add_movie GET Request handler
+    app.get('/add_movie', function (req, res, next) {
+        res.render('add_movie', {});
     });
 
+    // add_movie POST Request handler
+    app.post('/add_movie', function (req, res, next) {
+        var title = req.body.title;
+        var year = req.body.year;
+        var imdb = req.body.imdb;
+
+        if ((title == '') || (year == '') || (imdb == '')) {
+            next('Please provide an entry for all fields.');
+        } else {
+            db.collection('movies').insertOne(
+                {'title': title, 'year': year, 'imdb': imdb},
+                function (err, r) {
+                    assert.equal(null, err);
+                    res.send("Document inserted with _id: " + r.insertedId);
+                }
+            );
+        }
+    });
+
+    // Register Error Handler Middleware
+    app.use(errorHandler);
+
+    // Start Express Server
     var server = app.listen(3000, function () {
         var port = server.address().port;
         console.log('Express server listening on port %s.', port);
     });
 
 });
-
-
-
-
